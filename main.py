8b26d1824a4e9635e8081740640078af251ceca6
Daniel Shoun <danielshoun+servicesgithub@pm.me>
2020-10-28 21:27:40 -0400

    Added "/generate" command to create a reply regardless of probability.

--- main.py
+++ main.py
@@ -36,11 +36,23 @@ async def on_message(message):
             else:
                 probability = new_prob
                 if probability == 0:
-                    await mesage.channel.send("I'll stop talking completely until you change the probability again.")
+                    await mesage.channel.send("I'll stop talking completely until you change the probability again or say /generate.")
                 else:
                     await message.channel.send(f"I'll now generate text approximately 1 in every {probability} messages.")
-        except:
+        except ValueError:
             await message.channel.send("Could not parse a number from your message.")
+
+    elif message.content == "/generate":
+        length = random.randint(10, 100)
+        reply = gpt2.generate(sess, length=length, return_as_list=True)[0]
+        global possible_endings
+        latest_ending = -1
+        for ending in possible_endings:
+            if reply.rfind(ending) > latest_ending:
+                latest_ending = reply.rfind(ending)
+        if latest_ending != -1:
+            reply = reply[:latest_ending]
+        await message.channel.send(reply)
 
     else:
         if probability != 0:
